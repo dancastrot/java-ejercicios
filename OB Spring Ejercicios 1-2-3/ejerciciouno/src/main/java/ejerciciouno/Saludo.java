2659519cf4b3fd0a7b31303ca4a9f6be14ac019d
Daniel Castro <castrodanielt@hotmail.com>
2023-01-05 15:16:59 -0300

    actualizando contenido de rama

--- OB Spring Ejercicios 1-2-3/ejerciciouno/src/main/java/ejerciciouno/Saludo.java
+++
@@ -1,8 +0,0 @@
-package ejerciciouno;
-
-public class Saludo {
-	
-	public String imprimirSaludo() {
-		return "Buenos dias/tardes/noches";
-	}
-}
--- OBEjercicios12-13-14/src/metodoslimpios/Futbolista.java
+++
@@ -1,9 +0,0 @@
-package metodoslimpios;
-
-public class Futbolista {
-	
-	public String nombre;
-	public String apellido;
-	public String nacionalidad;
-	public String equipo;
-}
